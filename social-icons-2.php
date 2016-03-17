e059d37807164ecf132fa1717a169a7d76ab43b1
zinkoff <tosti@mikbook.local>
2016-03-17 09:56:33 +0100

    170316-0956

--- social-icons-2.php
+++ social-icons-2.php
@@ -1,15 +1,51 @@
-<style>
-#mc_embed_signup_danish{margin-left:30px;margin-top:10px;}
-#mc_embed_signup_english{margin-right:10px;margin-top:10px;}
-</style>
-<div class="clearfix">
-	<div class="small one_third">
-		<?php include "mailchimp-danish.php";?>
+<div class="clearfix social">
+
+
+	<div class="small one_third mc_embed_signup">
+		<div id="mc_embed_signup_danish">
+			<form action="//nielsborchjensen.us7.list-manage.com/subscribe/post?u=181cd183a1312b68b1915ddb5&amp;id=63b3156307" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<div id="mc_embed_signup_scroll1"  class="mc_embed_signup_scroll">
+					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email" required></input>
+<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					<span class="mc_embed_signup_span" aria-hidden="true">
+						<input type="text" name="b_181cd183a1312b68b1915ddb5_63b3156307" tabindex="-1" value=""></input>
+					</span>
+					<span class="clear">
+						<input type="submit" value="Nyhedsbrev (DK)" name="subscribe" id="mc-embedded-subscribe" class="button"></input>
+					</span>
+				</div>
+			</form>
+		</div>
 	</div>
-	<div class="small one_third" align="center">
-		<?php include "social-icons.php";?>
+	<div class="small one_third" align="center" class="social-icons">
+		<div align="center clearfix" id="social-icon-container">
+			<a href="https://vimeo.com/user39524819">
+				<span class="nbj-icons">s</span>
+			</a>
+			<a href="https://www.facebook.com/NielsBorchJensen/">
+				<span class="nbj-icons">b</span>
+			</a>
+			<a href="http://www.instagram.com/nielsborchjensen">
+				<span class="nbj-icons">x</span>
+			</a>
+		</div>
 	</div>
-	<div class="small one_third last">
-		<?php include "mailchimp-english.php";?>
+	<div class="small one_third last mc_embed_signup">
+	<div id="mc_embed_signup_english">
+		<form action="//nielsborchjensen.us7.list-manage.com/subscribe/post?u=f1d14450c580099f2975e09a6&amp;id=8dde27fe68" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form2" class="validate" target="_blank" novalidate>
+			<div id="mc_embed_signup_scroll2" class="mc_embed_signup_scroll">
+				<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email" required></input>
+<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				<span class="mc_embed_signup_span" aria-hidden="true">
+					<input type="text" name="b_f1d14450c580099f2975e09a6_8dde27fe68" tabindex="-1" value=""></input>
+				</span>
+				<span class="clear">
+					<input type="submit" value="Newsletter (EN)" name="subscribe" id="mc-embedded-subscribe" class="button"></input>
+				</span>
+			</div>
+		</form>
 	</div>
-<div class="clearboth"></div>
+
+
+</div>
+
